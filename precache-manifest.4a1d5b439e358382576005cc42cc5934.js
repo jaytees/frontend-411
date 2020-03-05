b9e29bfa9fd151fdfcdcf6e3d8415b9e6cbe3466
Jordan Trickett <jordan1trickett@gmail.com>
2020-03-05 21:31:22 +1100

    Updates

---
+++ precache-manifest.4a1d5b439e358382576005cc42cc5934.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057edf8284d1a9adfbf24dc831968951",
+    "url": "/frontend-slug/index.html"
+  },
+  {
+    "revision": "38d042e14440e9536921",
+    "url": "/frontend-slug/static/css/2.da6a58a2.chunk.css"
+  },
+  {
+    "revision": "71b1cfccd99eb729500d",
+    "url": "/frontend-slug/static/css/main.9a584a30.chunk.css"
+  },
+  {
+    "revision": "38d042e14440e9536921",
+    "url": "/frontend-slug/static/js/2.e81dd753.chunk.js"
+  },
+  {
+    "revision": "70f8db5419658ab0715227dc65c75b57",
+    "url": "/frontend-slug/static/js/2.e81dd753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b1cfccd99eb729500d",
+    "url": "/frontend-slug/static/js/main.3e169876.chunk.js"
+  },
+  {
+    "revision": "98bac05ce11e20e0369e",
+    "url": "/frontend-slug/static/js/runtime-main.c8e3af81.js"
+  },
+  {
+    "revision": "7f385716b929fa1ff1f684616bb4b6b3",
+    "url": "/frontend-slug/static/media/New-York-Times.7f385716.png"
+  },
+  {
+    "revision": "b30c3eeeb2b77facba48f9b00c39b133",
+    "url": "/frontend-slug/static/media/Pigeons-and-Planes.b30c3eee.png"
+  },
+  {
+    "revision": "644382c34eb4c6cac821f8a7ea9e2759",
+    "url": "/frontend-slug/static/media/slug.644382c3.png"
+  }
+]);
